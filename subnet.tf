5d6cfb40f87bcf4d07f711f435d60b123dd661cc
spy86 <maciej.michalsk@gmail.com>
2022-05-06 17:22:23 +0200

    fix

--- subnet.tf
+++ subnet.tf
@@ -3,5 +3,5 @@ resource "azurerm_subnet" "main" {
     count = "${length(var.subnet_prefix)}"
     resource_group_name = "${data.azurerm_resource_group.rg.name}"
     virtual_network_name = "${var.environment}-${var.vnet_name}-${var.region}-vnet"
-    address_prefix = "${lookup(element(var.subnet_prefix, count.index), "ip")}"
+    address_prefixes = "${lookup(element(var.subnet_prefix, count.index), "ip")}"
 }
