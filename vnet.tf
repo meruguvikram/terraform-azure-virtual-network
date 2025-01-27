94ae2ffb2e050782e8f057f4a7358182cd1b3c7e
Maciej Michalski <5445923+spy86@users.noreply.github.com>
2025-01-27 14:01:01 +0000

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,7 +1,8 @@
 resource "azurerm_virtual_network" "main" {
   name                = "${var.environment}-${var.vnet_name}-${var.region}-vnet"
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   address_space       = ["${var.vnet_address_space}"]
+  dns_servers         = var.vnet_custom_dns
   tags                = var.default_tags
 }
