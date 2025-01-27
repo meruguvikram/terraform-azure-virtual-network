bd2e3feaf774ae48042c1ce63a3c384f14eac514
Maciej Michalski <5445923+spy86@users.noreply.github.com>
2025-01-27 13:58:11 +0000

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,9 +1,21 @@
 resource "azurerm_subnet" "main" {
   for_each             = var.subnet_prefix
-  resource_group_name  = data.azurerm_resource_group.main.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = "${var.environment}-${var.vnet_name}-${var.region}-vnet"
   name                 = each.value["name"]
   address_prefixes     = each.value["ip"]
-
-  depends_on = [azurerm_virtual_network.main]
+  service_endpoints = [for se in lookup(each.value, "service_endpoints", []) : se.service]
+  dynamic "delegation" {
+    for_each = lookup(each.value, "delegations", [])
+    content {
+      name = delegation.value.name
+      service_delegation {
+        name = delegation.value.service_name
+        actions = delegation.value.actions
+      }
+    }
+  }
+  private_endpoint_network_policies = lookup(each.value, "private_endpoint_network_policies", null) 
+  private_link_service_network_policies_enabled = lookup(each.value, "private_link_service_network_policies_enabled", null)    
+depends_on = [azurerm_virtual_network.main]
 }
